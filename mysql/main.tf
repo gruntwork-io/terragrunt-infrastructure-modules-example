05911634ab74788c4b6040d86c0728f9f0be0690
Yevgeniy Brikman <brikis98@gmail.com>
2017-07-02 15:36:24 -0400

    Disable final snapshots

--- mysql/main.tf
+++ mysql/main.tf
@@ -30,4 +30,7 @@ resource "aws_db_instance" "mysql" {
   instance_class    = "${var.instance_class}"
   allocated_storage = "${var.allocated_storage}"
   storage_type      = "${var.storage_type}"
+
+  # TODO: DO NOT COPY THIS SETTING INTO YOUR PRODUCTION DBS. It's only here to make testing this code easier!
+  skip_final_snapshot = true
 }
