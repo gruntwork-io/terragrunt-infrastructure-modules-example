3d0fd2ffb1ac0df38d55e2a440eb5ee3c101856c
Yevgeniy Brikman <brikis98@gmail.com>
2017-07-02 15:28:15 -0400

    Add DB name to mysql module output

--- mysql/outputs.tf
+++ mysql/outputs.tf
@@ -2,6 +2,10 @@ output "endpoint" {
   value = "${aws_db_instance.mysql.endpoint}"
 }
 
+output "db_name" {
+  value = "${aws_db_instance.mysql.name}"
+}
+
 output "arn" {
   value = "${aws_db_instance.mysql.arn}"
 }
