703cb6e5e0ede967cc2c403f90e6bda19c72cb13
Travis Illig <tillig@paraesthesia.com>
2019-12-02 09:00:45 -0800

    Fixed interpolation-only string to remove warning.

--- asg-elb-service/main.tf
+++ asg-elb-service/main.tf
@@ -157,7 +157,7 @@ resource "aws_security_group_rule" "elb_allow_http_inbound" {
   to_port           = var.elb_port
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = "${aws_security_group.elb.id}"
+  security_group_id = aws_security_group.elb.id
 }
 
 resource "aws_security_group_rule" "elb_allow_all_outbound" {
