65cde8ce33b3ac9ed2519c62aa8709708469316a
Travis Illig <tillig@paraesthesia.com>
2019-12-03 08:44:32 -0800

    Pin Terraform 0.12.17.

--- mysql/main.tf
+++ mysql/main.tf
@@ -19,7 +19,7 @@ terraform {
 
   # Live modules pin exact Terraform version; generic modules let consumers pin the version.
   # The latest version of Terragrunt (v0.19.0 and above) requires Terraform 0.12.0 or above.
-  required_version = "= 0.12.16"
+  required_version = "= 0.12.17"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
