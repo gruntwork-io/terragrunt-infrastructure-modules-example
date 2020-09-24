0258e4ed3262d35d6e9671a1efdc452bdd719774
Rob Morgan <robbym@gmail.com>
2020-09-24 11:10:23 +0800

    lock provider plugin source

--- mysql/main.tf
+++ mysql/main.tf
@@ -13,7 +13,10 @@ terraform {
 
   # Live modules pin exact provider version; generic modules let consumers pin the version.
   required_providers {
-    aws = "= 3.7.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 3.7.0"
+    }
   }
 }
 
