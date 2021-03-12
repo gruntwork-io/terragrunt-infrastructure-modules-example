cb5b3856aa586f3b2e5db4e03653406db67150ec
Yevgeniy Brikman <brikis98@users.noreply.github.com>
2021-03-12 09:21:52 +0000

    Terraform v0.14 update

--- mysql/main.tf
+++ mysql/main.tf
@@ -9,7 +9,10 @@
 terraform {
   # Live modules pin exact Terraform version; generic modules let consumers pin the version.
   # The latest version of Terragrunt (v0.25.1 and above) recommends Terraform 0.13.3 or above.
-  required_version = "= 0.13.3"
+  # This module is now only being tested with Terraform 0.14.x. However, to make upgrading easier, we are setting
+  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
+  # forwards compatible with 0.14.x code.
+  required_version = ">= 0.12.26"
 
   # Live modules pin exact provider version; generic modules let consumers pin the version.
   required_providers {
