5779f6851338902fde4ccec9b79f0ed803e4801b
Gruntwork CI <ci@gruntwork.io>
2021-03-12 16:05:05 +0000

    Terraform v0.14 update (#22)
    * Terraform v0.14 update
    
    * Pin to exact 0.14.x version
    
    Co-authored-by: Yevgeniy Brikman <brikis98@users.noreply.github.com>

--- asg-elb-service/main.tf
+++ asg-elb-service/main.tf
@@ -10,8 +10,8 @@
 
 terraform {
   # Live modules pin exact Terraform version; generic modules let consumers pin the version.
-  # The latest version of Terragrunt (v0.25.1 and above) recommends Terraform 0.13.3 or above.
-  required_version = "= 0.13.3"
+  # The latest version of Terragrunt (v0.27.0 and above) recommends Terraform 0.14.0 or above.
+  required_version = "= 0.14.4"
 
   # Live modules pin exact provider version; generic modules let consumers pin the version.
   required_providers {
