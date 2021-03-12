a19a342f7e30b2d323141f9d676de10e69408966
Yevgeniy Brikman <brikis98@users.noreply.github.com>
2021-03-12 12:57:55 +0000

    Pin to exact 0.14.x version

--- asg-elb-service/main.tf
+++ asg-elb-service/main.tf
@@ -10,11 +10,8 @@
 
 terraform {
   # Live modules pin exact Terraform version; generic modules let consumers pin the version.
-  # The latest version of Terragrunt (v0.25.1 and above) recommends Terraform 0.13.3 or above.
-  # This module is now only being tested with Terraform 0.14.x. However, to make upgrading easier, we are setting
-  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
-  # forwards compatible with 0.14.x code.
-  required_version = ">= 0.12.26"
+  # The latest version of Terragrunt (v0.27.0 and above) recommends Terraform 0.14.0 or above.
+  required_version = "= 0.14.4"
 
   # Live modules pin exact provider version; generic modules let consumers pin the version.
   required_providers {
