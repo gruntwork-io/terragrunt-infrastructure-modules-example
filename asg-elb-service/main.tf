256fe9f6a64b3f7778150cc64e26c8dc2f6064b1
Yevgeniy Brikman <brikis98@users.noreply.github.com>
2021-04-27 13:47:24 +0100

    Fix version constraint

--- asg-elb-service/main.tf
+++ asg-elb-service/main.tf
@@ -11,7 +11,7 @@
 terraform {
   # Live modules pin exact Terraform version; generic modules let consumers pin the version.
   # The latest version of Terragrunt (v0.29.0 and above) recommends Terraform 0.15.0 or above.
-  required_version = ">= 0.12.26"
+  required_version = "= 0.15.0"
 
   # Live modules pin exact provider version; generic modules let consumers pin the version.
   required_providers {
