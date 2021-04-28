d40a8fe3fad8bc1a99270edee2efd684da57148f
Gruntwork CI <ci@gruntwork.io>
2021-04-28 08:29:42 +0100

    Update to Terraform 0.15 (#24)
    * Update to Terraform 0.15
    
    * Update to Terraform 0.15
    
    * Fix comment block
    
    * Fix version constraint
    
    Co-authored-by: Yevgeniy Brikman <brikis98@users.noreply.github.com>

--- asg-elb-service/main.tf
+++ asg-elb-service/main.tf
@@ -10,8 +10,8 @@
 
 terraform {
   # Live modules pin exact Terraform version; generic modules let consumers pin the version.
-  # The latest version of Terragrunt (v0.27.0 and above) recommends Terraform 0.14.0 or above.
-  required_version = "= 0.14.4"
+  # The latest version of Terragrunt (v0.29.0 and above) recommends Terraform 0.15.0 or above.
+  required_version = "= 0.15.0"
 
   # Live modules pin exact provider version; generic modules let consumers pin the version.
   required_providers {
