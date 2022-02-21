25af9b6686b7458c18532730e9bf0a93b3cd0f7a
Ana Krivokapić <ana@gruntwork.io>
2022-02-21 15:11:29 +0100

    Terraform 1.1 upgrade (#31)

--- mysql/main.tf
+++ mysql/main.tf
@@ -8,8 +8,8 @@
 
 terraform {
   # Live modules pin exact Terraform version; generic modules let consumers pin the version.
-  # The latest version of Terragrunt (v0.29.0 and above) recommends Terraform 0.15.0 or above.
-  required_version = "= 0.15.0"
+  # The latest version of Terragrunt (v0.36.0 and above) recommends Terraform 1.1.4 or above.
+  required_version = "= 1.1.4"
 
   # Live modules pin exact provider version; generic modules let consumers pin the version.
   required_providers {
