ba0e669cc3788d631986e31413d0fc8859b10e55
Rho <13165182+rhoboat@users.noreply.github.com>
2022-05-05 12:54:48 -0700

    Data source: use aws_subnets over aws_subnet_ids (#35)

--- asg-elb-service/dependencies.tf
+++ asg-elb-service/dependencies.tf
@@ -8,6 +8,9 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
